38dfdfa1c47103b4e55ea09cc93e4d03ff25909b
psvsai <pasupuletisvsm245@gmail.com>
2024-07-11 12:53:27 +0530

    cc

--- src/App.java
+++ src/App.java
@@ -3,8 +3,11 @@ public class App {
         System.out.println("Hello, World!");
         Car car1 = new Car();
         car1.run();
+        car1.horn();
         Car car2 = new Car();
-        car2.run();      
+        car2.run();  
+        Car car3 = new Car();
+        car3.run();  
 
     }
 }
--- src/Car.java
+++ src/Car.java
@@ -2,7 +2,10 @@ public class Car{
 
     public void run(){
         System.out.println("run");
+
     }
-    
+    public void horn(){
+        System.out.println("horn");
+    }
     
 }
