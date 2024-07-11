18a7f61f39ced3b981776999efe5e5481fcad241
psvsai <pasupuletisvsm245@gmail.com>
2024-07-11 12:46:10 +0530

    java

---
+++ src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        Car car1 = new Car();
+        car1.run();
+        Car car2 = new Car();
+        car2.run();      
+
+    }
+}
